ba8609564e1bc135e371dc80308ccc67e1ead7fa
satanabe1 <satoshi.watanabe.ssl@gmail.com>
2013-02-06 01:30:20 +0900

    古いGistを表示できない不具合を修正 ＆ ローディング画像のサイズと色を設定可能に

--- README.md
+++ README.md
@@ -6,17 +6,39 @@
 <script src="http://www.google.com/jsapi"></script>
 <script>google.load('jquery','1')</script>
 <script>(function(j){j(function(){$=jQuery=j})})($)</script>
-<script src="https://raw.github.com/sueno/G2AB/master/g2ab.js"></script>
+
+<script src="g2ab.js"></script>
 ```
 
 ブログでGistのソースコードを貼り付けたいところに，以下を記述
 
 ```
-<div class="amebloGist" id="貼り付けたいGistソースコードの番号(urlの数字部分)"></div>
+<div class="amebloGist" id="貼り付けたいGistソースコードの番号(urlの数字部分)">
+  url@GistのURL
+</div>
 ```
 
-example:
+例:
 
 ```
-<div class="amebloGist" id="0000000"></div>
+<div class="amebloGist">
+  url@https://gist.github.com/satanabe1/4683624
+</div>
 ```
+
+### オプションとか
+```
+<div class="amebloGist">
+  size@ローディング画像の大きさ
+  imgcolor@ローディング画像の基本色
+</div>
+```
+
+例:
+
+```
+<div class="amebloGist">
+  size@128
+  imgcolor@255,100,255
+</div>
+
--- g2ab.js
+++ g2ab.js
@@ -14,13 +14,13 @@ $(window).load(function(){
     function(targetDiv, canvasSize){
         var ldcanvas = new g2ab.LoadingCanvas(targetDiv);
 
-        ldcanvas.setSize = function(size){
-            this.size = size;
-            this.r = size / 2;
-            this.w = Math.round(size * 0.1);
-            this.h = Math.round(size * 0.25);
-            this.canvasElem.width = size;
-            this.canvasElem.height = size;
+        ldcanvas.setSize = function(_size){
+            this.size = _size;
+            this.r = _size / 2;
+            this.w = Math.round(_size * 0.1);
+            this.h = Math.round(_size * 0.25);
+            this.canvasElem.width = _size;
+            this.canvasElem.height = _size;
             this.canvasContext.setTransform(1,0,0,1,this.r,this.r);
         }
 
@@ -30,7 +30,9 @@ $(window).load(function(){
                 this.color[1] = g ;
                 this.color[2] = b ;
             }else if(arguments.length == 1){
-
+                var start = arguments[0].indexOf('(') + 1;
+                var end = arguments[0].indexOf(')');
+                this.color = arguments[0].substr(start , end - start).split(',');
             }else{
                 console.error('setColor' + arguments);
                 console.dir(arguments);
@@ -39,13 +41,13 @@ $(window).load(function(){
 
         ldcanvas.start = function(){
             this.loadingTimer = setInterval(g2ab.drawLoadingImage, 50, this);
-            console.dir(this);
         }
+
         ldcanvas.stop = function(){
             clearInterval(this.loadingTimer);
         }
-        ldcanvas.setSize(canvasSize);
 
+        ldcanvas.setSize(canvasSize);
         return ldcanvas;
     },
 LoadingCanvas :
@@ -68,42 +70,73 @@ drawLoadingImage :
             }
 
             ldcanvas.canvasContext.fillStyle = 'rgba(' + ldcanvas.color[0] + ',' + ldcanvas.color[1] + ',' + ldcanvas.color[2] + ',' + ldcanvas.alphas[i] + ')';
-            ldcanvas.canvasContext.strokeStyle = 'transparent';
-            ldcanvas.canvasContext.beginPath();
+                    ldcanvas.canvasContext.strokeStyle = 'transparent';
+                    ldcanvas.canvasContext.beginPath();
 
-            ldcanvas.canvasContext.moveTo((0-ldcanvas.w/4), (ldcanvas.r-ldcanvas.h));
-            ldcanvas.canvasContext.quadraticCurveTo(0, (ldcanvas.r-ldcanvas.h-ldcanvas.w/2),(0+ldcanvas.w/4),(ldcanvas.r-ldcanvas.h));
-            ldcanvas.canvasContext.lineTo((0+ldcanvas.w/2), (ldcanvas.r-ldcanvas.w/3));
-            ldcanvas.canvasContext.quadraticCurveTo(0, (ldcanvas.r+ldcanvas.w/3), (0-ldcanvas.w/2),(ldcanvas.r-ldcanvas.w/3));
-            ldcanvas.canvasContext.closePath();
-            ldcanvas.canvasContext.fill();
-            ldcanvas.canvasContext.stroke();
+                    ldcanvas.canvasContext.moveTo((0-ldcanvas.w/4), (ldcanvas.r-ldcanvas.h));
+                    ldcanvas.canvasContext.quadraticCurveTo(0, (ldcanvas.r-ldcanvas.h-ldcanvas.w/2),(0+ldcanvas.w/4),(ldcanvas.r-ldcanvas.h));
+                    ldcanvas.canvasContext.lineTo((0+ldcanvas.w/2), (ldcanvas.r-ldcanvas.w/3));
+                    ldcanvas.canvasContext.quadraticCurveTo(0, (ldcanvas.r+ldcanvas.w/3), (0-ldcanvas.w/2),(ldcanvas.r-ldcanvas.w/3));
+                    ldcanvas.canvasContext.closePath();
+                    ldcanvas.canvasContext.fill();
+                    ldcanvas.canvasContext.stroke();
 
-            ldcanvas.canvasContext.rotate((360/nodeC) * Math.PI / 180);
+                    ldcanvas.canvasContext.rotate((360/nodeC) * Math.PI / 180);
 
-        }
+                    }
 
-        ldcanvas.alphas.splice(0,0,ldcanvas.alphas[nodeC-1]).pop();
+                    ldcanvas.alphas.splice(0,0,ldcanvas.alphas[nodeC-1]).pop();
 
-    },
-    }
+                    },
+                    }
 
     $('div' + '.amebloGist').each(function(){
-        var param = $(this).attr('id');
-        if(param && param.match(/^[0-9]*$/)){
-            var gisturl = 'https://gist.github.com/'+param+'.json';
-            var gistcodeDiv = $('#' + param);
-            var ldcanvas = g2ab.createLoadingCanvas(gistcodeDiv.get(0), 180);
-            ldcanvas.setSize(40);
-            ldcanvas.setColor(55,55,255);
+        var keyValue = {};
+
+        $($(this).text().split("\n")).each(function(){
+            if(this.match(/^[ \t]*url@.+/)){ // url
+                var token = this.split('url@');
+                keyValue.url = token[1].replace(/[ \t]*$/,'');
+            }else if(this.match(/^[ \t]*type@.+/)){
+                var token = this.split('type@');
+                keyValue.type = token[1].replace(/[ \t]*$/,'');
+            }else if(this.match(/^[ \t]*size@.+/)){
+                var token = this.split('size@');
+                keyValue.size = token[1].replace(/[ \t]*$/,'');
+            }else if(this.match(/^[ \t]*imgcolor@.+/)){
+                var token = this.split('imgcolor@');
+                keyValue.imgcolor = 'rgb(' + token[1].replace(/[ \t]*$/,'') + ')';
+            }else if(this.match(/^[ \t]*fntcolor@.+/)){
+                var token = this.split('fntcolor@');
+                keyValue.fntcolor = 'rgb(' + token[1].replace(/[ \t]*$/,'') + ')';
+            }
+        });
+        console.dir(keyValue);
+
+        if(keyValue.url){
+            $(this).empty();
+
+            var gisturl = keyValue.url;
+            var gistcodeDiv = $(this);
+            var ldcanvas = g2ab.createLoadingCanvas(gistcodeDiv.get(0), 30);
+
+            if(keyValue.size){
+                ldcanvas.setSize(keyValue.size);
+            }
+            if(keyValue.imgcolor){
+                ldcanvas.setColor(keyValue.imgcolor);
+            }
+
             ldcanvas.start();
 
-            $('<span>' + gisturl +'</span>').appendTo(gistcodeDiv);
+            $('<span />').appendTo(gistcodeDiv).text('Loading : ');
+            $('<a />').appendTo(gistcodeDiv).attr('href',gisturl).attr('target','_blank').text(gisturl);
 
             $.ajax({
-                url: gisturl,
+                url: gisturl + '.json',
                 type: 'GET',
-                dataType: 'jsonp'
+                dataType: 'jsonp',
+                cache: false
             }).success(function(gistdata) {
                 // link要素の追加
                 $('<link />',{
@@ -114,12 +147,14 @@ drawLoadingImage :
                 ldcanvas.stop();
                 // gistコードの追加
                 gistcodeDiv.html(gistdata.div);
-            }).error(function() {
-                alert("error")
+            }).error(function(ex) {
+                console.log(ex);
             });
         }else{
-            notice(param, "Illigal parameter.");
+            console.error("Illigal parameter.");
+            console.dir(keyValue);
+            //alert("Illigal parameter.");
         }
     });
-});
+    });
 
